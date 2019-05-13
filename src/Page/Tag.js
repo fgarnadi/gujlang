4147a3cad9f7cb884adda46e5883c32a4f1ebe90
Bicky Eric Kantona <bickyeric@ymail.com>
2019-05-13 22:41:52 +0700

    page screen

---
+++ src/Page/Tag.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Tag() {
+  return (
+    <div class="after-post-tags">
+      <ul class="tags">
+        <li><a href="#">Design</a></li>
+        <li><a href="#">Growth Mindset</a></li>
+        <li><a href="#">Productivity</a></li>
+        <li><a href="#">Personal Growth</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Tag;
