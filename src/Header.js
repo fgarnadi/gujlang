4970f1f7cdb127e8f4cad3d7c207490db29bb6cb
fgarnadi <fajar.garnadi@gmail.com>
2020-03-27 02:13:47 +0700

    Update react-script and add eslint

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-class Header extends React.Component{
-  render(){
+class Header extends React.PureComponent {
+  render() {
     return (
       <header className="tl_article_header">
         <h1 dir="auto">One Piece 942</h1>
         <address dir="auto">
-          <a href="/" rel="author">MANGACANBLOG</a>
+          <a href="/" rel="author">
+            MANGACANBLOG
+          </a>
           <time dateTime="2019-05-17T16:13:15+0000">May 17, 2019</time>
         </address>
       </header>
-    )
+    );
   }
 }
 
